75f642fde9f5e9bbd482f62ae173b69c36d4d317
sanjanavarade <127649474+sanjanavarade@users.noreply.github.com>
2025-07-06 21:27:29 +0530

    feat: update dependencies and refactor Tailwind CSS configuration
    - Added new dependencies: @radix-ui/react-dialog, chart.js, query-string, react-chartjs-2, react-countup, and autoprefixer.
    - Updated tailwindcss to version 3.4.17 and removed the old tailwind.config.ts file.
    - Refactored postcss.config.mjs to include autoprefixer.
    - Created new components: AnimatedCounter, BankCard, DoughnutChart, HeaderBox, MobileNav, RightSidebar, Sidebar, TotalBalanceBox, and Sheet UI components.
    - Implemented new pages for my-banks, payment-transfer, and transaction-history.
    - Updated tsconfig.json to include tailwind.config.js.
    - Enhanced the layout and styling of components to improve user experience.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,46 @@
+import HeaderBox from '@/components/HeaderBox';
+import RightSidebar from '@/components/RightSidebar';
+import TotalBalanceBox from '@/components/TotalBalanceBox';
 import React from 'react'
 
 const Home = () => {
+
+  const loggedIn={
+    firstName:'Sanjana'
+  };
   return (
-    <div>
-      Home
-    </div>
+      <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox 
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+
+           <TotalBalanceBox 
+            // accounts={accountsData}
+            // totalBanks={accounts?.totalBanks}
+            // totalCurrentBalance={accounts?.totalCurrentBalance}
+          />
+        </header>
+
+        {/*<RecentTransactions 
+          accounts={accountsData}
+          transactions={account?.transactions}
+          appwriteItemId={appwriteItemId}
+          page={currentPage}
+        />
+      </div>*/
+
+      <RightSidebar 
+        user={loggedIn}
+        transactions={account?.transactions}
+        banks={accountsData?.slice(0, 2)}
+      /> }
+      </div>
+    </section>
   )
 }
 
