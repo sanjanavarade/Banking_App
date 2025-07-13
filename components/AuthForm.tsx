0159bd88747abbee062eaf1ad2bc858d5a520240
sanjanavarade <127649474+sanjanavarade@users.noreply.github.com>
2025-07-13 19:07:12 +0530

    Implement plaidLink and Dwolla

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -21,7 +21,7 @@ import { Input } from "@/components/ui/input"
 import CustomInput from './CustomInput';
 import { authFormSchema } from '@/lib/utils';
 import { Loader2 } from 'lucide-react';
-import { useRouter } from 'next/navigation';'
+import { useRouter } from 'next/navigation';
 import { getLoggedInUser, signIn, signUp } from '@/lib/actions/user.actions';
 import PlaidLink from './PlaidLink';
 
